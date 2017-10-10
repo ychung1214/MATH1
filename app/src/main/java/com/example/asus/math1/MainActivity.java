e207d9d5f0416bcc898ee1a906232f62afeee6eb
chian9512 <chian9512@gmail.com>
2017-10-11 01:25:55 +0800

    10/11數學加法減法拖曳未完成

---
+++ app/src/main/java/com/example/asus/math1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.asus.math1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button add = (Button)findViewById(R.id.add);
+        add.setOnClickListener(
+                new Button.OnClickListener()
+                {
+                    public void onClick(View v)
+                    {
+                        Intent myIntent = new Intent(v.getContext(), addition.class);
+                        startActivity(myIntent);
+                    }
+                }
+        );
+
+    }
+}
